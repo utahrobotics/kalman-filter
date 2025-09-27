5515446c167797b59609b5b6b80cea95f7d8adda
Hydro111 <67488614+Hydro111@users.noreply.github.com>
2025-09-27 12:02:55 -0600

    Stub utils file

---
+++ src/kalman_filter_utils.rs
@@ -0,0 +1,6 @@
+use crate::kalman_filter::*;
+
+// TODO: (from issue #2)
+// * Replacing evolution function pointer with matrix.
+// * Replacing full covariance matrix with diagonal covariance matrix.
+// * Allowing a constant dt to be set on creation of filter struct.
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,5 @@
 mod kalman_filter;
 pub use kalman_filter::*;
+
+mod kalman_filter_utils;
+pub use kalman_filter_utils::*;
