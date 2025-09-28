3820dc126c6e10786e0f5f0081406267db03fb46
Hydro111 <67488614+Hydro111@users.noreply.github.com>
2025-09-27 18:11:00 -0600

    Add simple utilities for interacting with library
    Resolves #2 Add simple wrappers

--- src/kalman_filter_utils.rs
+++ src/kalman_filter_utils.rs
@@ -4,3 +4,17 @@ use crate::kalman_filter::*;
 // * Replacing evolution function pointer with matrix.
 // * Replacing full covariance matrix with diagonal covariance matrix.
 // * Allowing a constant dt to be set on creation of filter struct.
+
+
+
+/// Create a covariance matrix from the variances of each variable, stored in
+/// a vector. This assumes the variables are completely independent.
+pub fn covariance_matrix_from_variance_vector<const SIZE: usize>(variances: &SimpleVector<SIZE>) -> SimpleSquareMatrix<SIZE> {
+	SimpleSquareMatrix::from_diagonal(variances)
+}
+
+/// Create a covariance matrix from one variance value. This assumes the 
+/// variables are completely independent, and equally variant.
+pub fn covariance_matrix_from_variance<const SIZE: usize>(variance: f64) -> SimpleSquareMatrix<SIZE> {
+	SimpleSquareMatrix::from_diagonal_element(variance)
+}
